a2e55038cea438a72b8d882db674c7b7d6ba2dc8
unknown <yamini.pandey@ankit11mayoutlook.onmicrosoft.com>
2025-06-29 22:53:38 +0530

    new1

--- main.tf
+++ main.tf
@@ -20,3 +20,26 @@ resource "azurerm_resource_group" "example4" {
   name     = "example-resources123"
   location = "West Europe"
 }
+
+
+resource "azure_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = "example-resources"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+ 
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+
+
+resource "azurerm_resource_group" "example5" {
+  name     = "example-resources1234"
+  location = "West Europe"
+}
