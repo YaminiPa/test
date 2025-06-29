69ca3bcf8fd32139c5fce4fc986750f49386c090
unknown <yamini.pandey@ankit11mayoutlook.onmicrosoft.com>
2025-06-29 22:52:23 +0530

    new

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 resource "azure_storage_account" "example" {
   name                     = "examplestorageacct"
   resource_group_name      = "example-resources"
@@ -13,7 +14,9 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example2" {
-  name     = "example-resources1"
+
+
+resource "azurerm_resource_group" "example4" {
+  name     = "example-resources123"
   location = "West Europe"
 }
